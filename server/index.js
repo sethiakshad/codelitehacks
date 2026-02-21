60c9bb88063b6219d58bf9300ac3b7963dc7bffc
Akshad Sethi <akshadsethi123@gmail.com>
2026-02-21 15:18:38 +0530

    port

--- server/index.js
+++ server/index.js
@@ -9,6 +9,6 @@ const app = express();
 app.use(express.json());
 app.use("/api", userRoutes);
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 4000, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
